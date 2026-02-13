73382cf64d1344ceca338c3a718c26910911aef4
ahmed elselly <ahmedelselly87@gmail.com>
2026-02-13 13:52:42 +0200

    added plan module

--- src/plan/plan.module.ts
+++ src/plan/plan.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PlanService } from './plan.service';
 import { PlanController } from './plan.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Plan } from './plan.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Plan])],
   providers: [PlanService],
-  controllers: [PlanController]
+  controllers: [PlanController],
 })
 export class PlanModule {}
